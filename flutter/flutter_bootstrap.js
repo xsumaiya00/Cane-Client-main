2ecd1535adf2e50b70f7c44c47d776e5e680a0ef
xsumaiya00 <xshahis00@vutbr.cz>
2025-04-21 10:52:40 +0000

    Deploying to gh-page from @ xsumaiya00/Cane-Client-main@47b381ae66e5ed68e79c40c967c93c536424cf06 🚀

--- flutter/flutter_bootstrap.js
+++ flutter/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3452345661"
+    serviceWorkerVersion: "932705087"
   }
 });
